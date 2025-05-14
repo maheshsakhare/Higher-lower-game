3928e933db0197412e2792e3518f2f3a290aaaa6
maheshsakhare <sakharemahesh035@gmail.com>
2025-05-14 22:03:38 +0530

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,92 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 627,  # In millions
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 398,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 507,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 429,
+        "description": "Singer and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 396,
+        "description": "Actor and professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 381,
+        "description": "Singer and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 363,
+        "description": "Reality TV star and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 328,
+        "description": "Singer",
+        "country": "United States"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 270,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 286,
+        "description": "Singer-songwriter",
+        "country": "United States"
+    },
+    {
+        "name": "National Geographic",
+        "follower_count": 283,
+        "description": "Magazine",
+        "country": "United States"
+    },
+    {
+        "name": "Nike",
+        "follower_count": 315,
+        "description": "Sportswear brand",
+        "country": "United States"
+    },
+    {
+        "name": "Instagram",
+        "follower_count": 674,
+        "description": "Social media platform",
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 292,
+        "description": "Singer",
+        "country": "Canada"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 294,
+        "description": "Model",
+        "country": "United States"
+    }
+]
